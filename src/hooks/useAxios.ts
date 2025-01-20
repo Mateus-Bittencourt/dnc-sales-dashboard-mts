3a012a5396ae21432a2c1f33937a7aa69db19775
Mateus Bittencourt <mateusmbittencourt@gmail.com>
2025-01-19 22:19:29 -0300

    feat: set components in the home and create get hook

--- src/hooks/useAxios.ts
+++ src/hooks/useAxios.ts
@@ -1,5 +1,6 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import axios, { AxiosRequestConfig } from 'axios'
+import Cookies from 'js-cookie'
 
 const axiosInstance = axios.create({
   baseURL: `${import.meta.env.VITE_API_BASE_URL}/`,
@@ -34,4 +35,39 @@ export const usePost = <T, P>(endpoint: string) => {
   }
 
   return { data, loading, error, postData }
+}
+
+const useGet = (endpoint: string, config?: AxiosRequestConfig) => {
+  const [data, setData] = useState<any | null>(null)
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState<number | null>(null)
+
+  const getData = async () => {
+    setData(null)
+    setLoading(true)
+    setError(null)
+
+    try {
+      const response = await axiosInstance({
+        url: endpoint,
+        method: 'get',
+        headers: {
+          Authorization: `Bearer ${Cookies.get('Authorization')}`,
+          ...config?.headers,
+        },
+        ...config,
+      })
+      setData(response.data)
+    } catch (error: any) {
+      setError(error.response.status ?? 500)
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  useEffect(() => {
+    getData()
+  }, [])
+
+  return { data, loading, error, getData }
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,8 +4,9 @@ import {
   CustomChart,
   CustomTable,
   Header,
+  StyledH2,
 } from '@/components'
-import { Container } from '@mui/material'
+import { Container, Grid } from '@mui/material'
 import { currencyConverter } from '@/utils'
 
 function Home() {
@@ -40,23 +41,61 @@ function Home() {
     <>
       <Header />
       <Container maxWidth="lg">
-        <CardComponent className="">Card</CardComponent>
-        <CardComponent className="">
-          <AvatarList listData={mockListData} />
-        </CardComponent>
-        <CardComponent className="">
-          <CustomTable
-            headers={mockTableData.headers}
-            rows={mockTableData.rows}
-          />
-        </CardComponent>
-        <CardComponent className="">
-          <CustomChart
-            data={[10, 20, 30, 40, 20]}
-            labels={['Jan', 'Feb', 'Mar', 'Apr', 'May']}
-            type="bar"
-          />
-        </CardComponent>
+        <Grid container spacing={4}>
+          <Grid item xs={12} md={4}>
+            <CardComponent>
+              {/* <StyledH2 className='mb-1'>Total Revenue</StyledH2> */}
+              <StyledH2 className="mb-1">Total sales for the month</StyledH2>
+            </CardComponent>
+          </Grid>
+          <Grid item xs={12} md={4}>
+            <CardComponent>
+              <StyledH2 className="mb-1">Goal of the month</StyledH2>
+            </CardComponent>
+          </Grid>
+          <Grid item xs={12} md={4}>
+            <CardComponent>
+              <StyledH2 className="mb-1">Leads contacted</StyledH2>
+            </CardComponent>
+          </Grid>
+
+          <Grid item xs={12} md={7}>
+            <CardComponent>
+              <StyledH2 className="mb-1">Sales value in the month</StyledH2>
+              <CustomChart
+                data={[10, 20, 30, 40, 20]}
+                labels={['Jan', 'Feb', 'Mar', 'Apr', 'May']}
+                type="line"
+              />
+            </CardComponent>
+          </Grid>
+          <Grid item xs={12} md={5}>
+            <CardComponent>
+              <StyledH2 className="mb-1">Top sellers of the month</StyledH2>
+              <AvatarList listData={mockListData} />
+            </CardComponent>
+          </Grid>
+
+          <Grid item xs={12} md={5}>
+            <CardComponent>
+              <StyledH2 className="mb-1">Relevant news</StyledH2>
+              <CustomTable
+                headers={mockTableData.headers}
+                rows={mockTableData.rows}
+              />
+            </CardComponent>
+          </Grid>
+          <Grid item xs={12} md={7}>
+            <CardComponent>
+              <StyledH2 className="mb-1">Sales value per month</StyledH2>
+              <CustomChart
+                data={[10, 20, 30, 40, 20]}
+                labels={['Jan', 'Feb', 'Mar', 'Apr', 'May']}
+                type="bar"
+              />
+            </CardComponent>
+          </Grid>
+        </Grid>
       </Container>
       <h1>Home</h1>
     </>
